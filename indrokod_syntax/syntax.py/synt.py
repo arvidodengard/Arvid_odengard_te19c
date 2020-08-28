34b424a09e5064b5088097b5b767f7a8f3894614
arvidodengard <70264523+arvidodengard@users.noreply.github.com>
2020-08-28 09:39:09 +0200

    intro

--- indrokod_syntax/syntax.py/synt.py
+++ indrokod_syntax/syntax.py/synt.py
@@ -7,4 +7,4 @@ print(f"Ditt namn är {name}")
 side = float(input("Ange kvadratens sida: "))
 area = side**2
 
-print(f"Arean av kvadraten är {area} a.e.")
+print(f"Arean av kvadraten är {area} a.e.") # print
