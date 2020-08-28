74a446d6af3f7c6327b8839ccb0667ba774a4fa2
arvidodengard <70264523+arvidodengard@users.noreply.github.com>
2020-08-28 09:37:51 +0200

    indrodiktionskod

---
+++ indrokod_syntax/syntax.py/synt.py
@@ -0,0 +1,10 @@
+print("hej te19c")
+
+# skapar en variabel och tilldelar det värdet "Arvid"
+name = "Arvid"
+print(f"Ditt namn är {name}")
+
+side = float(input("Ange kvadratens sida: "))
+area = side**2
+
+print(f"Arean av kvadraten är {area} a.e.")
