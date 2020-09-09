8a482cf43f6cd428d8996bc2499ec3526b6e6922
arvidodengard <70264523+arvidodengard@users.noreply.github.com>
2020-09-09 13:55:14 +0200

    programering läxa

--- .vscode/programering/prog.py
+++ .vscode/programering/prog.py
@@ -4,23 +4,33 @@ print(f"Ditt namn är {name}")
 g = 9.82
 
 
-m = float(input("77"))
+m = float(input("din vikt"))
 
 F = m*g # 599.02
 
 print(f"Din tyngdkraft är {F}N") #f-string 
 print("Din tyngdkraft är",F,"N")
 
-decimalTal = float(input("0,45: "))
+decimalTal = float(input("decimaltal"))
 procent = decimalTal*100
 
 print(f"{decimalTal} = {procent:.1f}%")
 print(decimalTal,"=",round(procent),"%")
 
-k = float(input("18"))
-c = k - 273.15
-print(f"antal celsius")
+kelvin = float(input("antal kelvin"))
+c = kelvin - 273.15
+print(f"antal celsius{c}7")
 
-c = float(input("18"))
+c = float(input("antal celsius"))
 k = c + 273.15
-print(f"antal kelvin")
+print(f"antal kelvin {k}")
+
+a = float(input("hur många gånger per månad åker du med västrafik"))
+
+b = a*30
+
+if b > 775:
+    print("månadskort blir billigare")
+
+
+print(f"priset blir {b}")
