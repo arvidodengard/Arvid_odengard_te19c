fccd9f15d52469b59bfbd885bf4692eb1150e33b
arvidodengard <70264523+arvidodengard@users.noreply.github.com>
2020-09-09 13:12:50 +0200

    website

---
+++ .vscode/programering/prog.py
@@ -0,0 +1,26 @@
+name = "Arvid"
+print(f"Ditt namn är {name}")
+
+g = 9.82
+
+
+m = float(input("77"))
+
+F = m*g # 599.02
+
+print(f"Din tyngdkraft är {F}N") #f-string 
+print("Din tyngdkraft är",F,"N")
+
+decimalTal = float(input("0,45: "))
+procent = decimalTal*100
+
+print(f"{decimalTal} = {procent:.1f}%")
+print(decimalTal,"=",round(procent),"%")
+
+k = float(input("18"))
+c = k - 273.15
+print(f"antal celsius")
+
+c = float(input("18"))
+k = c + 273.15
+print(f"antal kelvin")
