5c15c6f4acf3f92649c759b8fdd8a6c16fda20d5
arvidodengard <70264523+arvidodengard@users.noreply.github.com>
2020-09-16 13:51:10 +0200

    laxa och webb

---
+++ .vscode/laxa2.py
@@ -0,0 +1,45 @@
+a = float(input("valfritt tal"))
+
+if a > 0:
+    print("possetivt")
+
+if a == 0:
+    print("noll")
+
+if a < 0:
+    print("negativt")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+a = float(input("hur många grader"))
+
+
+
+if a == 90:
+    print("rät vinkel")
+
+if a > 90 < 180:
+    print("trubig vinkel")
+
+if a < 90:
+    print("spetsig vinkel")
+
+if a == 180:
+    print("rak vinkel")
+
+if a > 180:
+    print("konvex vinkel")
+
+if a == 360:
+    print("hel vinkel")
