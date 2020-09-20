3f8276aad8e33d339e059e8f3aacff16b7fa1761
arvidodengard <70264523+arvidodengard@users.noreply.github.com>
2020-09-20 20:37:06 +0200

    background bilder till cover

--- .vscode/laxa2.py
+++ .vscode/laxa2.py
@@ -1,45 +1,62 @@
+# Uppgift 1
+
 a = float(input("valfritt tal"))
 
 if a > 0:
     print("possetivt")
 
-if a == 0:
+elif a == 0:
     print("noll")
 
-if a < 0:
+elif a < 0:
     print("negativt")
 
 
-
-
-
-
-
-
-
-
-
+# Uppgift 4
 
 
 
 a = float(input("hur många grader"))
 
-
+if a < 0:
+    print("ej definerat")
 
 if a == 90:
     print("rät vinkel")
 
-if a > 90 < 180:
+elif a > 90 and a < 180:
     print("trubig vinkel")
 
-if a < 90:
+elif a > 0 and a < 90:
     print("spetsig vinkel")
 
-if a == 180:
+elif a == 180:
     print("rak vinkel")
 
-if a > 180:
+elif a > 180 and a < 360:
     print("konvex vinkel")
 
-if a == 360:
+elif a == 360:
     print("hel vinkel")
+
+    
+    # Uppgift 5
+    
+
+    r = float(input("tal på radien"))
+
+    if r > 0
+        c = r*2*3.14
+
+    elif r < 0
+
+    if r < 0 
+        print("ej definerat")
+    
+    else r > 0
+        print("Radien är possetiv")
+
+    
+ 
+
+    print("cirkens har omkretsen {cirkens omkrets}")
