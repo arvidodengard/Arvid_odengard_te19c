f6f2bd33964866235de251c2461bbf067080f4a9
arvidodengard <70264523+arvidodengard@users.noreply.github.com>
2020-10-01 23:08:11 +0200

    klar med läxa 3

---
+++ .vscode/for_satser/for_satser.py
@@ -0,0 +1,14 @@
+# skriv ut alla tal 1-100
+
+for i in range(1,101):
+    print(i, end =" ")
+
+for j in range(101,49,-1):
+    print(j,end =" ")
+
+s = 0
+for i in range(11):
+    s += i
+
+    print(f"1+2+3+...+9+10 = {s}")
+
---
+++ .vscode/for_satser/forsatser.py
@@ -0,0 +1,34 @@
+# 1) a.b.c
+for i in range(1,11):
+    print(i, end =" ")
+print("\n")
+
+for i in range(-11,11):
+    print(i, end =" ")
+print("\n")
+for i in range(11,0,-1):
+    print(i, end =" ")
+print("\n")
+# uppgift 3
+s = 0 
+for i in range(0,101):
+    s += i
+
+# uppgift 4)
+print(f"1.2.3+...+99+100 är {s}")
+print("\n")
+s = 0 
+for i in range(0,100,2):
+    s += i
+print(f"1.3.5+...+99+ är {s}")
+print("\n")
+
+# upggift 5) a.
+for i in range(1,11):
+    i*=5
+    print(i, end=" ")
+print("\n")
+    
+
+ 
+
