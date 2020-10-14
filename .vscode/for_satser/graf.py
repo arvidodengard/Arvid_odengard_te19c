3f7231bd09c3b6a85f59769b752217b087525acd
arvidodengard <70264523+arvidodengard@users.noreply.github.com>
2020-10-14 13:35:00 +0200

    test

---
+++ .vscode/for_satser/graf.py
@@ -0,0 +1,10 @@
+import matplotlib. pyplot as plt
+
+s = [10,20,30,40,50,60,70,80,90,100]
+t = [1.83, 2.87, 3.78, 4.65, 5.5, 6.32, 7.14^, 7.96, 8.79, 9.69]
+
+plt.plot(t,s,)
+plt.title("Usian Bolt - s-t graf 100m")
+plt.xlabel ("Tid i sekunder")
+plt.ylabel("sträcka i meter")
+plt.show()
