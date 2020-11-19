7791b4e7b91f98dd3eeb3fc344558ef3ff244f49
arvidodengard <70264523+arvidodengard@users.noreply.github.com>
2020-11-19 23:58:12 +0100

    klar med läxan

--- .vscode/inlämning v45/stringläxa.py
+++ .vscode/inlämning v45/stringläxa.py
@@ -3,13 +3,32 @@ namn = "Arvid"
 print(f"Ditt namn innehåller {len(namn)} bokstäver")
 
 #Uppgift 2
-mening = "En bild säger tusen mer än tusen ord, en matematisk formel säger mer än tusen bilder"
+mening = "En bild säger mer än tusen ord, en matematisk formel säger mer än tusen bilder"
 
 mening = mening.split()
 print(f"meningen innehåller {len(mening)} ord")
 
-#Uppgift 3
+#Uppgift 3a
 
-s = (input("din mening"))
+ord = (input("din mening: "))
 
-print(f"bokstav på index 0: {s[1]}")
+if ord == ord[::-1]:
+    print("är ett palindrom")
+
+else:
+    print("ej ett palindrom")
+
+#Uppgift 3b
+ord = input("Mata in en mening av tecken med mellanslag och skiljetecken")
+ord = ord.replace (" ", "")
+ord = ord.replace (",", "")
+ord = ord.replace (".", "")
+
+if ord[::-1] == ord:
+    print("Är ett palindrom")
+else:
+    print("Är ej ett palindrom")
+
+
+
+
